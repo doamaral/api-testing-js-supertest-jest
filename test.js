1d825c0290bda7a59627bd29d72d0064b60f3134
doamaral <lopesdoamaral@gmail.com>
2020-09-23 21:31:07 -0300

    Using diverse expectation sources, including chai

--- test.js
+++ test.js
@@ -1,14 +1,18 @@
+const chai = require("chai");
+const expectation = chai.expect;
 request = require('supertest');
 const SERVER = "http://localhost";
 const URL = SERVER + ":3000";
 console.log(URL);
 
-describe("GET /produtos", ()=>{
+describe("Using supertest native expectation", ()=>{
   //using expect from the supertest API. Expectation over the response status
   it("Status should be 200", (done) => {
     const response = request(URL).get("/produtos").expect(200).end(done);
   });
+});
 
+describe("Using jest expectation", ()=>{
   //using expect from jest. Expectation over the response status
   it("Status should be 200", async () => {
     const response = await request(URL).get("/produtos");
@@ -19,5 +23,13 @@ describe("GET /produtos", ()=>{
   it("Quantidade should be 2", async () => {
     const response = await request(URL).get("/produtos");
     expect(response.body.quantidade).toBe(2);
+  });
+});
+
+describe("Using chai expectation", ()=>{
+  //using expect from chai. Expectation over a response body field
+  it("Quantidade should be 2", async () => {
+    const response = await request(URL).get("/produtos");
+    expectation(response.body.quantidade).to.eql(2);
   });
 });
